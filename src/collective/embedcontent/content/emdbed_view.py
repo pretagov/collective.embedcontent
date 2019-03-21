427d9ea62056e6ef9664029d36fb85044150315e
quang.nguyen <quang@pretagov.com>
2019-03-21 17:25:47 +1100

    Init addon

--- src/collective/embedcontent/content/emdbed_view.py
+++ src/collective/embedcontent/content/emdbed_view.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from zope import schema
+from plone.namedfile.field import NamedFile
 
 
 class IEmdbedView(model.Schema):
     """ Marker interface for EmdbedView
     """
+    zip_file = NamedFile(title=(u'ZIP file'))
+
+    htnl_source = schema.NamedFile(
+        title=(u'HTML Package'),
+    )
