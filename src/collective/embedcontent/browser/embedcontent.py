e8e6c2264b68b6427ea008ea7d23546e1adafbd0
quang.nguyen <quang@pretagov.com>
2019-04-17 15:28:41 +1000

    Add tile embed

--- src/collective/embedcontent/browser/embedcontent.py
+++ src/collective/embedcontent/browser/embedcontent.py
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
 from plone.dexterity.browser.view import DefaultView
-from plone.dexterity.browser.edit import DefaultEditForm
-from plone.dexterity.browser.add import DefaultAddForm, DefaultAddView
-from z3c.form import interfaces
 from zope.interface import implements, Interface
 from zope.publisher.interfaces import IPublishTraverse
-from zope import component
 from Products.Five import BrowserView
-from Products.CMFCore.utils import getToolByName
 from BTrees.OOBTree import OOBTree
+from plone.app.standardtiles.existingcontent import uuidToObject
+from plone.uuid.interfaces import IUUID
+from zExceptions import Unauthorized
+from plone.tiles.tile import Tile
+from plone.app.tiles.browser.edit import DefaultEditView, DefaultEditForm
+from plone.app.tiles.browser.add import DefaultAddView, DefaultAddForm
+from plone.app.tiles.browser.delete import DefaultDeleteView, DefaultDeleteForm
+from plone.dexterity.utils import createContentInContainer
+from zope.lifecycleevent import ObjectModifiedEvent, ObjectCreatedEvent
+from plone.namedfile.utils import get_contenttype
+from plone.app.textfield.value import RichTextValue
+from plone.namedfile.file import NamedBlobFile
+import zope.event
 import urllib
 
+def embedContentPackageUrl(content):
+    randomID = getattr(content, 'contentHash', None)
+    return '%s/@@contents/%s/%s' % (content.absolute_url(), randomID, content.index_file)
 
 class EmbedContentView(DefaultView):
 
     def package_url(self):
-        randomID = getattr(self.context, 'contentHash', None)
-        return '%s/@@contents/%s/%s' % (self.context.absolute_url(), randomID, self.context.index_file)
+        return embedContentPackageUrl(self.context)
 
+    def content(self):
+        return self
 
 class PublishableString(str):
     """Zope will publish this since it has a __doc__ string"""
@@ -30,7 +42,7 @@ class PublishableString(str):
 
 
 
-class EmbedContentContentView(DefaultView):
+class EmbedContentContentView(BrowserView):
     """ @@contents browser view to access zipfile's contents
         """
     implements(IPublishTraverse)
@@ -57,14 +69,104 @@ class EmbedContentContentView(DefaultView):
         request.RESPONSE.setHeader('content-type', zipTree.content_type)
         return PublishableString(zipTree)
 
-class EmbedContentEditForm(DefaultEditForm):
-    pass
+
+def saveTileDataToEmbedContent(form):
+    if not form.tileId:
+        return
+    embed_content_id = '%s-%s-EmbedContent' % (form.tileType.__name__, form.tileId)
+    embed_content = getattr(form.context, embed_content_id, None)
+    if not embed_content:
+        embed_content = createContentInContainer(form.context.aq_parent, "EmbedContent", title=embed_content_id)
+        setattr(form.context, embed_content_id, embed_content)
+    embed_content.html_content = RichTextValue(form.request.get('%s.html_content' % form.tileType.__name__))
+    embed_content.index_file = form.request.get('%s.index_file' % form.tileType.__name__)
+    if form.request.get('%s.package_content' % form.tileType.__name__):
+        package_file = form.request.get('%s.package_content' % form.tileType.__name__)
+        package_file.seek(0)
+        filename = package_file.filename
+        contenttype = get_contenttype(filename=filename)
+        data = package_file.read()
+        embed_content.package_content = NamedBlobFile(data, contenttype, unicode(filename))
+    else:
+        action =  form.request.get('%s.package_content.action' % form.tileType.__name__)
+        if action =='remove':
+            embed_content.package_content = None
+    return embed_content
+
+class EmbedContentTileEditForm(DefaultEditForm):
+
+    def extractData(self):
+        embed_content = saveTileDataToEmbedContent(self)
+        if embed_content:
+            zope.event.notify(ObjectModifiedEvent(embed_content))
+        data, errors =  DefaultEditForm.extractData(self)
+        # Remove blob from data as it is not supported by tile
+        if 'package_content' in data:
+            del data['package_content']
+        return (data,errors)
+
+    def getContent(self):
+        content = DefaultEditForm.getContent(self)
+        embed_content_id = '%s-%s-EmbedContent' % (self.tileType.__name__, self.tileId)
+        embed_content = getattr(self.context, embed_content_id, None)
+        if embed_content:
+            content['package_content'] = embed_content.package_content
+            content['html_content'] = embed_content.html_content
+            content['index_file'] = embed_content.index_file
+        return content
 
 
+class EmbedContentTileEdit(DefaultEditView):
+    form = EmbedContentTileEditForm
 
-class EmbedContentAddForm(DefaultAddForm):
-    portal_type = 'EmbedContent'
+class EmbedContentTileAddForm(DefaultAddForm):
+
+    def extractData(self):
+        import pdb
+        pdb.set_trace()
+        embed_content = saveTileDataToEmbedContent(self)
+        if embed_content:
+            zope.event.notify(ObjectCreatedEvent(embed_content))
+        # Remove blob from data as it is not supported by tile
+        data, errors = DefaultAddForm.extractData(self)
+        if 'package_content' in data:
+            del data['package_content']
+        return (data, errors)
+
+class EmbedContentTileAdd(DefaultAddView):
+    form = EmbedContentTileAddForm
 
 
-class EmbedContentAddView(DefaultAddView):
-    form = EmbedContentAddForm
+class EmbedContentTileDeleteForm(DefaultDeleteForm):
+
+    def extractData(self):
+        embed_content_id = '%s-%s-EmbedContent' % (self.tileType.__name__, self.tileId)
+        embed_content = getattr(self.context, embed_content_id, None)
+        if embed_content:
+            parent = self.context.aq_parent
+            parent.manage_delObjects(embed_content.id)
+        data, errors = DefaultDeleteForm.extractData(self)
+        # Remove blob from data as it is not supported by tile
+        if 'package_content' in data:
+            del data['package_content']
+        return (data, errors)
+
+class EmbedContentTileDelete(DefaultDeleteView):
+    form = EmbedContentTileDeleteForm
+
+class EmbedContentTile(Tile):
+    """ A tile for mosaic representing a embed content """
+
+    @property
+    def context_content(self):
+        embed_content_id = '%s-%s-EmbedContent' % (self.__name__, self.id)
+        embed_content = getattr(self.context, embed_content_id, None)
+        content = {'package_content':'','html_content':'','index_file':'','package_url':''}
+        if embed_content:
+            content['package_content'] = embed_content.package_content
+            content['html_content'] = embed_content.html_content
+            content['index_file'] = embed_content.index_file
+            content['package_url'] = embedContentPackageUrl(embed_content)
+        return content
+
+
--- src/collective/embedcontent/content/embedcontent.py
+++ src/collective/embedcontent/content/embedcontent.py
@@ -4,15 +4,16 @@ from plone.supermodel import model
 from zope import schema
 from plone.namedfile.field import NamedBlobFile
 from plone.app.textfield import RichText
+from z3c.form.interfaces import IObjectFactory
+from z3c.form.object import FactoryAdapter, getIfName
+from zope.component import queryUtility
+from plone.app.vocabularies.catalog import CatalogSource
+from zope.component.interfaces import IFactory, implements
+import zope.component
 
 class IEmbedContent(model.Schema):
     """ Marker interface for EmbedContent
     """
-    package_content = NamedBlobFile(
-        title=(u'Package Content'),
-        description=(u'Package content'),
-        required=False,
-    )
 
     html_content = RichText(
         title=(u'HTML Content'),
@@ -20,7 +21,13 @@ class IEmbedContent(model.Schema):
         required=False,
     )
 
-    index_file = schema.Text(
+    package_content = NamedBlobFile(
+        title=(u'Package Content'),
+        description=(u'Package content'),
+        required=False,
+    )
+
+    index_file = schema.TextLine(
         title=(u'Index file'),
         description=(u'Index file in package content'),
         default=(u'index.html'),
