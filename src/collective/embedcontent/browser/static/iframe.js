9278c6c2101949c6d8ecf19a4657dcf733f12ec4
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-02-21 23:55:26 +0000

    Fix jquery

--- src/collective/embedcontent/browser/static/iframe.js
+++ src/collective/embedcontent/browser/static/iframe.js
@@ -1,5 +1,5 @@
  $(document).ready(function () {
-        $("iframe.embed-content-frame").load(function () {
-            $(this).height($(this).contents().find("html").height());
-        });
+        $("iframe.embed-content-frame").on( "load", function () {
+          $(this).height($(this).contents().find("html").height());
+      });
     });
